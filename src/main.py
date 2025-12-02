46ad9824a885cc99c888dafe0ea70e42302a9203
brandim04 <brandimlarissa.sa@gmail.com>
2025-12-01 23:07:16 -0300

    Refactor database connection code in main.py

--- src/main.py
+++ src/main.py
@@ -2,17 +2,14 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env
 load_dotenv()
 
-# Fetch variables
 USER = os.getenv("user")
 PASSWORD = os.getenv("password")
 HOST = os.getenv("host")
 PORT = os.getenv("port")
 DBNAME = os.getenv("dbname")
 
-# Connect to the database
 try:
     connection = psycopg2.connect(
         user=USER,
@@ -23,15 +20,12 @@ try:
     )
     print("Connection successful!")
     
-    # Create a cursor to execute SQL queries
     cursor = connection.cursor()
     
-    # Example query
     cursor.execute("SELECT NOW();")
     result = cursor.fetchone()
     print("Current Time:", result)
 
-    # Close the cursor and connection
     cursor.close()
     connection.close()
     print("Connection closed.")
